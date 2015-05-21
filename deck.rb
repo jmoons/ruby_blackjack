2aedd8df4ef6aa20174b6f258c0eaba863a8a740
John Moons <j.moons@realdigitalmedia.com>
2015-05-21 11:04:02 -0400

    added deck and shuffled deck to Deck class

--- deck.rb
+++ deck.rb
@@ -1,7 +1,17 @@
+require './card.rb'
+
 class Deck
-  def initialize(args)
-    
+  SUITS   = ["Clubs", "Diamonds", "Hearts", "Spades"]
+  NUMBERS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+
+  def initialize
+    @deck           = get_full_deck.flatten
+    @shuffled_deck  = @deck.shuffle
   end
-  
-  
+
+  private
+
+  def get_full_deck
+    SUITS.map{ |suit| NUMBERS.map{ |number| Card.new(suit: suit, number: number) } }
+  end
 end
