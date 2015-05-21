382caa83c66edc83506ba10b5e282eaf78345c60
John Moons <j.moons@realdigitalmedia.com>
2015-05-21 17:24:21 -0400

    can now play a single hand

--- blackjack.rb
+++ blackjack.rb
@@ -8,25 +8,113 @@ class Blackjack
 
   def initialize(number_of_decks: 1)
     @game_deck    = generate_game_deck(number_of_decks)
+    @mutable_deck = @game_deck.dup
     @keep_playing = true
+    @current_hand = {:player => [], :house => []}
     play_hand
   end
   
   private
 
   def play_hand
-    mutable_deck = @game_deck.dup
-    current_hand = {:player => [], :house => []}
-    current_hand = deal_cards(mutable_deck, 2, current_hand)
-    display_status(current_hand)
+    deal_initial_cards
+    # Show cards
+    # Prompt user for action
+    # Hit - deal a card
+    # Stand - play dealers hand
+    display_status_hidden_dealer_card
+
+    if play_players_hand
+      #player busted
+      print_busted_message("player")
+      return
+    end
+
+    display_status_full
+
+    if play_dealers_hand
+      print_busted_message("dealer")
+      return
+    end
+
+    determine_winner
+
   end
 
-  def display_status(current_hand)
-    puts "YOU: #{current_hand[:player]}"
-    puts "YOU: #{card_hand_sum(current_hand[:player])}"
-    puts "House: #{current_hand[:house].inspect}"
-    puts "House: #{card_hand_sum(current_hand[:house])}"
+  def determine_winner
+    if ( card_hand_sum(@current_hand[:player]) > card_hand_sum(@current_hand[:house]) )
+      puts "Player Wins!"
+    else
+      puts "House Wins - DOH"
+    end
+  end
 
+  def print_busted_message(who_busted)
+    puts "#{who_busted} BUSTED."
+  end
+
+  def play_dealers_hand
+    if card_hand_sum(@current_hand[:house]) < 17
+      deal_card(:house)
+      display_status_full
+      return busted?(:house) if busted?(:house)
+      play_dealers_hand
+    else
+      return false
+    end
+  end
+
+  def play_players_hand
+
+    puts "Hit or Stand?"
+    response = gets.chomp.downcase
+    if response == "h"
+      deal_card(:player)
+      display_status_hidden_dealer_card
+      return busted?(:player) if busted?(:player)
+      play_players_hand
+    elsif response == "s"
+      return false
+    else
+      play_players_hand
+    end
+  end
+
+  def deal_initial_cards
+    (1 .. 2).each do |card|
+      deal_card(:player)
+      deal_card(:house)
+    end
+
+  end
+
+  def prompt_player
+    puts "Hit or Stand?"
+    gets.chomp.downcase
+  end
+
+  def display_status_full
+    puts "YOU: #{@current_hand[:player]}"
+    puts "YOU: #{card_hand_sum(@current_hand[:player])}"
+    puts "House: #{@current_hand[:house].inspect}"
+    puts "House: #{card_hand_sum(@current_hand[:house])}"
+
+  end
+
+  def display_status_hidden_dealer_card
+    puts "YOU: #{@current_hand[:player]}"
+    puts "YOU: #{card_hand_sum(@current_hand[:player])}"
+    puts "House: #{[@current_hand[:house][1]].inspect}"
+    puts "House: #{card_hand_sum([@current_hand[:house][1]])}"
+
+  end
+
+  def busted?(target)
+    if card_hand_sum(@current_hand[target]) > 21
+      return true
+    end
+
+    return false
   end
 
   def card_hand_sum(card_hand)
@@ -58,12 +146,8 @@ class Blackjack
     end
   end
 
-  def deal_cards(mutable_deck, number_of_cards_to_deal, current_hand)
-    (1 .. number_of_cards_to_deal).each do |card_deal|
-      current_hand[:player] << mutable_deck.shift
-      current_hand[:house] << mutable_deck.shift
-    end
-    current_hand
+  def deal_card(target)
+    @current_hand[target] << @mutable_deck.shift
   end
 
   def prompt_to_continue
